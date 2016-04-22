e86dbf1eddcf9bcd62b61260cad9138f4b6be1a3
Douglas Nomizo <douglasnomizo@outlook.com>
2016-04-22 14:38:38 -0300

    Small refactor - missing tests =(

--- src/clojure_swapi/core.clj
+++ src/clojure_swapi/core.clj
@@ -5,17 +5,31 @@
 
 (def swapi "http://swapi.co/api/")
 (def people (str swapi "people/"))
+(def vehicles (str swapi "vehicles/"))
+
+(defn get-json
+  [url]
+  (json/read-str (:body (client/get url))))
 
 (defn person 
   [identifier]
-  (json/read-str (:body (client/get (str people identifier)))))
+  (get-json (str people identifier)))
 
 (def mperson (memoize person))
 
+(defn by
+  [type collection]
+  (group-by #(get % (name type)) collection))
+
+(defn people-by
+  [type identification-list]
+  (comp (by type (map mperson identification-list))))
+
+(defn vehicle
+  [identifier]
+  (get-json (str vehicles identifier)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println (time (person 1)))
-  (println (time (person 1)))
-  (println (time (mperson 1)))
-  (println (time (mperson 1))))
+  (def identification-list '(1,2,5))
+  (clojure.pprint/pprint (people-by :species identification-list)))
