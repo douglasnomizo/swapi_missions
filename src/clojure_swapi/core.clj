6ed0f0ae023db374b637181eb39a23e26f27b272
Douglas Nomizo <douglasnomizo@outlook.com>
2017-07-31 15:18:56 -0300

    wip

--- src/clojure_swapi/core.clj
+++ src/clojure_swapi/core.clj
@@ -1,7 +1,40 @@
 (ns clojure-swapi.core
-  (:require [clojure-swapi.io.client :as swapi])
-  (:require [clojure-swapi.domain.api :as core])
+  (:require [clj-http.client :as client])
+  (:require [clojure.data.json :as json])
   (:gen-class))
+
+(def resources #{ "films", "people", "planets", "species", "starships", "vehicles" })
+(def swapi "http://swapi.co/api/")
+(def people_resource (str swapi "people/"))
+(def species_resource (str swapi "species/"))
+(def vehicles_resource (str swapi "vehicles/"))
+(def http-get #(client/get %))
+
+(defn get-json
+  [url]
+  (json/read-str (:body (http-get url))))
+
+(def mget-json (memoize get-json))
+
+(defn person
+  [identifier]
+  (mget-json (str people_resource identifier)))
+
+(defn vehicle
+  [identifier]
+  (mget-json (str vehicles_resource identifier)))
+
+(defn species
+  [identifier]
+  (mget-json (str species_resource identifier)))
+
+(defn by
+  [type collection]
+  (group-by #(get % (name type)) collection))
+
+(defn people-by
+  [type people]
+  (comp (by type people)))
 
 
 (defn keywords
@@ -23,7 +56,7 @@
   (update person "species"
     (fn [species]
       (vec
-        (map #(get % "name") (map swapi/mget-json species))))))
+        (map #(get % "name") (map mget-json species))))))
 
 (defn ceil-division
   [numerator denominator]
@@ -34,11 +67,11 @@
   [& args]
 
   (def identification-list '(1, 2, 5, 5, 62, 71, 72))
-  (def allience-tank (swapi/vehicle "72"))
-  (def people (map #(select-keys % ["name", "species", "mass"]) (map swapi/person identification-list)))
+  (def allience-tank (vehicle "72"))
+  (def people (map #(select-keys % ["name", "species", "mass"]) (map person identification-list)))
   (def updated-people-mass (map update-mass people))
   (def updated-people-species (map update-species people))
-  (def by-species (core/people-by :species updated-people-species))
+  (def by-species (people-by :species updated-people-species))
 
   (clojure.pprint/pprint "List by species with name, species and mass properties")
   (clojure.pprint/pprint by-species)
